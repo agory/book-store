ac3b124acb3e5e52f10ac90071310bc659f2c6b9
agory <gory.alexandre02@gmail.com>
2018-09-04 14:46:01 +0200

    feature(backend): add BookRepository and Book model

---
+++ src/main/java/com/example/bookstore/entities/Book.java
@@ -0,0 +1,69 @@
+package com.example.bookstore.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    private String isbn;
+
+    private String title;
+
+    private String authors;
+
+    private String publisher;
+
+    private String image;
+
+    private String description;
+
+    @Id
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

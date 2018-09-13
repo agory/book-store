1cb31d1797d52888dfd997bd5b5c04c764292463
agory <gory.alexandre02@gmail.com>
2018-09-13 14:36:50 +0200

    test(backend): add dependance to fix test error when jdk > 9

--- src/test/java/com/example/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookStoreApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.bookstore;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookStoreApplicationTests {
+
+	@BeforeClass
+	public static void switchOffLiquibase() {
+		System.setProperty("spring.liquibase.enabled", "false");
+	}
+
 
 	@Test
 	public void contextLoads() {

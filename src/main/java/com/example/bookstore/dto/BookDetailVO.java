28592809f74b3abb085f2c8064300f69f77257d1
GoryAlexandre <gory.alexandre02@gmail.com>
2018-09-16 10:22:00 +0200

    add publish date and add order by on findAll  (#15)
    * feat(backend): Add Authenfication
    
    Create User and Role Entity (db, repository, facade)
    
    * test(backend): add dependance to fix test error when jdk > 9
    
    * feat(cli): add travis and jenkins
    
    * feat(backend): add publish date and add order by on findAll

--- src/main/java/com/example/bookstore/dto/BookDetailVO.java
+++ src/main/java/com/example/bookstore/dto/BookDetailVO.java
@@ -3,15 +3,17 @@ package com.example.bookstore.dto;
 import com.example.bookstore.entities.Book;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class BookDetailVO implements Serializable {
 
-    final String isbn;
-    final String title;
-    final String authors;
-    final String publisher;
-    final String image;
-    final String description;
+    private final String isbn;
+    private final String title;
+    private final String authors;
+    private final String publisher;
+    private final String image;
+    private final String description;
+    private final Date publishDate;
 
     public BookDetailVO(Book book) {
         this.isbn = book.getIsbn();
@@ -20,6 +22,7 @@ public class BookDetailVO implements Serializable {
         this.publisher = book.getPublisher();
         this.image = book.getImage();
         this.description = book.getDescription();
+        this.publishDate = book.getPublishDate();
     }
 
     public String getIsbn() {
@@ -44,5 +47,9 @@ public class BookDetailVO implements Serializable {
 
     public String getDescription() {
         return description;
+    }
+
+    public Date getPublishDate() {
+        return publishDate;
     }
 }
